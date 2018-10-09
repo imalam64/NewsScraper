2ece22a583a2ef6ef2b385e2a9d883692d1b1cd5
imalam64 <imalam64@gmail.com>
2018-10-09 16:20:30 -0700

    added server and routes

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const handlebars = require("express-handlebars");
+const bodyparser = require("body-parser")
+
+
+const router = require('./router/routes');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+app.use(express.static("public"));
+
+// Setup Handlebars
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Connect to the Mongo DB
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/curbedScraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+//Use the routes
+app.use('/', router);
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
